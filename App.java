9a93de7ff312d25bc1acf75a68e9a64289e6b474
vibhaj1234 <vibhaj132@gamil.com>
2025-06-05 19:38:26 +0530

    afternoon

---
+++ App.java
@@ -0,0 +1,13 @@
+package devops.exp10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
